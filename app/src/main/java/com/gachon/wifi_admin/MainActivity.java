af493ae40fb6839d8cf5e33767f842fd8355d237
Hongsi-Taste <kwonwk123@gmail.com>
2022-05-15 14:28:10 +0900

    scan&res success

--- app/src/main/java/com/gachon/wifi_admin/MainActivity.java
+++ app/src/main/java/com/gachon/wifi_admin/MainActivity.java
@@ -1,14 +1,145 @@
 package com.gachon.wifi_admin;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    WifiManager wifiManager;
+    TextView test_tv;
+    Button wifi_scan_btn;
+    String sep_str = ", SSID";
+
+    private void scanSuccess() {
+        List<ScanResult> results = wifiManager.getScanResults();
+        Log.e("wifi-info",results.toString());
+        String scan_res = results.toString();
+        String scan_list= "";
+        String[] scan_sep = scan_res.split(sep_str);
+        for(int i=0;i<scan_sep.length;i++){
+            scan_sep[i] += "\n\nSSID: ";
+            scan_list += scan_sep[i];
+        }
+        test_tv.setText(scan_list);
+    }
+
+    private void scanFailure() {
+        // handle failure: new scan did NOT succeed
+        // consider using old scan results: these are the OLD results!
+        List<ScanResult> results = wifiManager.getScanResults();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        test_tv = findViewById(R.id.res_v1);
+        wifi_scan_btn = findViewById(R.id.req_wifiinfo);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            Log.d("permission","checkSelfPermission");
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                Log.d("permission","shouldShowRequestPermissionRationale");
+                // 사용자에게 설명을 보여줍니다.
+                // 권한 요청을 다시 시도합니다.
+
+            } else {
+                // 권한요청
+
+                Log.d("permission","권한 요청");
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_WIFI_STATE,Manifest.permission.CHANGE_WIFI_STATE},
+                        1000);
+
+            }
+        }
+
+        wifiManager = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context c, Intent intent) {
+                boolean success = intent.getBooleanExtra(
+                        WifiManager.EXTRA_RESULTS_UPDATED, false);
+                if (success) {
+                    scanSuccess();
+                    Log.e("wifi","scanSuccess !!!!!!!!!!!!!!!");
+                } else {
+                    // scan failure handling
+                    scanFailure();
+                    Log.e("wifi","scanFailure ..............");
+                }
+            }
+        };
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        registerReceiver(wifiScanReceiver, intentFilter);
+
+
+        wifi_scan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean success = wifiManager.startScan();
+                if (!success) {
+                    // scan failure handling
+                    scanFailure();
+                    Log.e("wifi","scanFailure ..............");
+                }
+            }
+        });
+
+    }
+
+    //권한요청을 사용자에게 허락받았는지 못받았는지 결과를 알수 있는 콜백 메서드
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case 1000: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // 권한 획득 성공
+                    Log.d("permission","권한 획득 성공");
+
+                } else {
+
+                    // 권한 획득 실패
+                    Log.d("permission","권한 획득 실패");
+                }
+                return;
+            }
+
+        }
     }
 }
